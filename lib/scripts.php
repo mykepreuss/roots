bb332a46ad80a19de3198c8832477390c5264246
Myke Preuss <myke@signals.ca>
2013-12-20 10:22:49 -0800

    reconfiguring dependecies to work with the assembly-wp yeoman generator

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,10 +11,8 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6c39f42987ae297a5a21e2bb35bf3402');
-
-  wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '01d073428e4e54f227f8a0a3bad4b804', true);
+  //enqueuing modernizr
+  wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/bower_components/modernizr.js', false, null, false);
 
   wp_enqueue_script('modernizr');
 }
--- templates/head.php
+++ templates/head.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<!--[if IE]>	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<![endif]-->
+	<title><?php wp_title('|', true, 'right'); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php bloginfo('description') ?>">
+	<link rel="stylesheet" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<?php wp_head(); ?>
 
-  <?php wp_head(); ?>
-
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+	<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">	
 </head>
