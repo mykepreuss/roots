d849af4bdaab82b5e1eca531c4ba933190226513
Michael Barrow <michael@assemblydigital.com>
2015-06-23 11:25:10 -0700

    remove assets function

--- lib/assets.php
+++ lib/assets.php
@@ -65,14 +65,14 @@ function asset_path($filename) {
   }
 }
 
-function assets() {
-  wp_enqueue_style('sage_css', asset_path('styles/main.css'), false, null);
+// function assets() {
+//   wp_enqueue_style('sage_css', asset_path('styles/main.css'), false, null);
 
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
+//   if (is_single() && comments_open() && get_option('thread_comments')) {
+//     wp_enqueue_script('comment-reply');
+//   }
 
-  wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
-  wp_enqueue_script('sage_js', asset_path('scripts/main.js'), ['jquery'], null, true);
-}
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
+//   wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
+//   wp_enqueue_script('sage_js', asset_path('scripts/main.js'), ['jquery'], null, true);
+// }
+// add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
